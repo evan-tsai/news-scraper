72e7fa1ecbad68ad4db7bc18674333acb4099fe3
Evan Tsai <evan@err7.net>
2019-07-18 14:45:02 +0800

    Add helmet & cors

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import express from 'express';
 import { connectDb } from './models';
 import scrape from './services/scrape';
 import logger from './helpers/logger';
+import cors from 'cors';
+import helmet from 'helmet';
 import routes from './routes';
 import cron from 'node-cron';
 const app = express();
 
+app.use(cors());
+app.use(helmet());
 app.use('/', routes);
 
 connectDb().then(async () => {
